2fb2c5cf096fef0743d83973075c532fd5ff1995
Igor <igorprzyblski127@gmail.com>
2024-02-07 13:47:11 +0100

    devise
    view for visits and patients

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_154105) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_07_142646) do
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "phone"
     t.string "specialization"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.string "topic"
+    t.string "patient"
+    t.string "doctor"
+    t.datetime "begining"
+    t.datetime "ending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
